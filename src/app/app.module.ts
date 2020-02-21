407e4dac556ee10db17da0525cac79558ccadc4b
Israel Ribas <israel.ribas@unochapeco.edu.br>
2020-02-21 14:01:47 -0300

    Academia Jedi

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
